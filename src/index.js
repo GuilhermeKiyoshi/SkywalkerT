474c8ac360bc7764a71e3c1978969e28c8e3f185
GuilhermeKiyoshi <kiyoshi.guilherme@gmail.com>
2020-08-01 14:25:49 -0300

    Setup inicial do Projeto

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const Pagina404 = () =>  (<div>404</div>)
+ 
+ReactDOM.render(
+  <BrowserRouter>
+
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={Pagina404} />
+    </Switch>
+
+  </BrowserRouter>,
+  document.getElementById('root')
+);
---
+++ src/pages/cadastro/Video/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CadastroVideo(){
+    return (
+      <div>
+        Pagina teste
+      </div>
+    )
+  }
+
+  export default CadastroVideo;
