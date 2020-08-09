0d304dc884f986d94e7bb8b0f698a1624b67a1cd
GuilhermeKiyoshi <kiyoshi.guilherme@gmail.com>
2020-08-09 20:23:57 -0300

    Atualização Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import { FooterBase } from './styles';
+import { FooterBase } from '../Footer/styles';
+import Logo from '../../assets/img/Logo.png';
+import IconLinkedin from '../../assets/img/linkedin.png';
+import IconGithub from '../../assets/img/github.png';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="/">
+        <img className="Logo" src={Logo} alt="Logo Hiroflix" />
       </a>
       <p>
-        Orgulhosamente criado durante a
-        {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
-        </a>
+        Guilherme Kiyoshi Mazaro
+
+        <p style={{ display: 'flex', justifyContent: 'center' }}>
+            <a target="_blank" href="https://www.linkedin.com/in/luis-mazaro-781260136/"><img alt="Linkedin" style={{ width: '32px', margin: '4px' }} src={IconLinkedin} /></a>
+            <a target="_blank" href="https://github.com/Mazaro-bot"><img alt="Github" style={{ width: '32px', margin: '4px' }} src={IconGithub} /></a>
+        </p>
+
+        <p style={{ fontSize: '.6em' }}>
+
+          {' '}
+          <a href="https://www.alura.com.br/">Feito na Imersão React da Alura</a>
+        </p>
+
       </p>
     </FooterBase>
   );
