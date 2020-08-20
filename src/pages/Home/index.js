2ae509c557b18984030c5ea9c0a292f9c151054a
GuilhermeKiyoshi <kiyoshi.guilherme@gmail.com>
2020-08-20 19:37:48 -0300

    readme

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-// import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 import categoriasRepository from '../../repositores/categorias';
@@ -47,27 +46,7 @@ function Home() {
         );
       })}
 
-      {/* <BannerMain
-      videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-      url={dadosIniciais.categorias[0].videos[0].url}
-      videoDescription="O que"
-    />
-    <Carousel
-      ignoreFirstVideo
-      category={dadosIniciais.categorias[0]}
-    />
-    <Carousel
-      category={dadosIniciais.categorias[1]}
-    />
-    <Carousel
-      category={dadosIniciais.categorias[2]}
-    />
-    <Carousel
-      category={dadosIniciais.categorias[3]}
-    />
-    <Carousel
-      category={dadosIniciais.categorias[4]}
-    /> */}
+
     </PageDefault>
   );
 }
