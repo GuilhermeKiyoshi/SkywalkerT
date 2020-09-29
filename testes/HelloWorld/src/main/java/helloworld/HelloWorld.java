a7e8eb535c9af3f9a7e66b8ac7c942255e6c22ae
GuilhermeKiyoshi <kiyoshi.guilherme@gmail.com>
2020-09-28 21:07:29 -0300

    correções

---
+++ testes/HelloWorld/src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,34 @@
+
+package helloworld;
+
+
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        
+        byte Byte = 7;
+        short Short = 4;
+        int Int = 5;
+        long Long = 899999988222222l;
+        
+        float Float = 3.9222f;
+        double Double = 6.3;
+        
+        char Char = 'X';
+        String string = "string";
+        
+        boolean Boolean = false;
+        
+        
+        System.out.println(Byte + " " +  Short);
+        System.out.println(Short);
+        System.out.println(Int);
+        System.out.println(Long);
+        System.out.println(Float);
+        System.out.println(Double);
+        System.out.println(Char);
+        System.out.println(string);
+        System.out.println(Boolean);
+       
+    }                  
+}
